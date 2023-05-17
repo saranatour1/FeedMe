f050e8c85fde3bbb84fee9e545b25356c4ceadae
Sara <saranatourcontact@gmail.com>
2023-05-17 12:37:19 +0300

    Login and Regestration Backend

---
+++ Feedme/src/test/java/com/codingdojo/feedme/FeedmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.feedme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
