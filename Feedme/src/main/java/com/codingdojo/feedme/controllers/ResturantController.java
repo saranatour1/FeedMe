9ee8d6f69a80faaf1df010cb6cb3a97b9e15cadd
Sara <saranatourcontact@gmail.com>
2023-05-18 13:56:27 +0300

    a few error resolving details

--- Feedme/src/main/java/com/codingdojo/feedme/controllers/ResturantController.java
+++ Feedme/src/main/java/com/codingdojo/feedme/controllers/ResturantController.java
@@ -1,14 +1,19 @@
 package com.codingdojo.feedme.controllers;
 
+import java.util.List;
+
 import org.eclipse.tags.shaded.org.apache.xpath.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.codingdojo.feedme.models.Resturant;
 import com.codingdojo.feedme.models.User;
 import com.codingdojo.feedme.services.OrderServices;
+import com.codingdojo.feedme.services.RatingServices;
 import com.codingdojo.feedme.services.ResturantServices;
 import com.codingdojo.feedme.services.UserService;
 
@@ -16,19 +21,19 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ResturantController {
-  //user services 
+	// user services
 
-  @Autowired
+	@Autowired
 	private UserService userServ;
 
-  @Autowired
+	@Autowired
 	private ResturantServices restServ;
 
 	@Autowired
 	private OrderServices orderServ;
-  
-  @Autowired 
-  private RatingServices rateServ;
+
+	@Autowired
+	private RatingServices rateServ;
 
 	@RequestMapping("/")
 	public String aboutUs(Model model) {
@@ -43,41 +48,38 @@ public class ResturantController {
 		model.addAttribute("usercount", userCount);
 		return "index.jsp";
 	}
-  
-  	@GetMapping("/resturants")
+
+	// to render the main page after regestration
+	@GetMapping("/resturants")
 	public String dashbord(Model model, HttpSession session) {
-		//to collect all resturants
-    Long newUserId = (Long) session.getAttribute("newUser");
+		// to collect all resturants
+		Long newUserId = (Long) session.getAttribute("newUser");
 		User thisUser = userServ.findUserById(newUserId);
 		model.addAttribute("thisUser", thisUser);
-		List<Resturant> allrest=restServ.findAllResturants();
-		//to collect all rating 
-		//List<Rating> rate=rateServices.findAllRating();
-		List<Object[]> rating= rateServ.findAverageStarsPerRestaurant();
-		model.addAttribute("all_rest", allrest); // all resturants 
-		model.addAttribute("all_rating", rating); //rating 
-  
-			
+		List<Resturant> allrest = restServ.findAllResturants();
+		// to collect all rating
+		// List<Rating> rate=rateServices.findAllRating();
+		List<Object[]> rating = rateServ.findAverageStarsPerRestaurant();
+		model.addAttribute("all_rest", allrest); // all resturants
+		model.addAttribute("all_rating", rating); // rating
 		return "rest.jsp";
 	}
-  	
-	//this to show single Resturant information 
+
+	// this to show single Resturant information
 	@GetMapping("/resturantss/{rest_id}")
-	public String singleResturant(Model model,@PathVariable("rest_id") Long id) {
-		Resturant resturant=restServ.findRestById(id);
-		//to find a single resturant 
-		double avg=rateServ.findAverageRatingForRestaurant(id);
-		model.addAttribute("rest",resturant);
+	public String singleResturant(Model model, @PathVariable("rest_id") Long id) {
+		Resturant resturant = restServ.findRestById(id);
+		// to find a single resturant
+		double avg = rateServ.findAverageRatingForRestaurant(id);
+		model.addAttribute("rest", resturant);
 
-		return "show_rest_information.jsp";		
+		return "show_rest_information.jsp";
 	}
-
 
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		session.removeAttribute("newUser");
 		return "redirect:/";
 	}
-
 
 }
